33352477d3009d1dc1612c3f23e87a118ed1b470
vberi786 <100026333+vberi786@users.noreply.github.com>
2023-04-20 14:39:58 +0530

    done

---
+++ footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+   padding: 5px 0px 5px 0px;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <p>© Copyright 2023<span id="demo">
+    <a href="ishar4150@gmail.com">
+  </span> <span class="brand">ASRS By </span> All Rights Reserved.
+</footer></p>
+</div>
+
+</body>
+
+
+</html> 
